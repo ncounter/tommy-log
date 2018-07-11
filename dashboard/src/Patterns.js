777782e7c77e6c0a2340061f1b84eae0d79c2ffd
dleidi <dleidi@suse.com>
2018-07-11 15:40:08 +0200

    Adapt UI to digest new patterns data

--- dashboard/src/Patterns.js
+++ dashboard/src/Patterns.js
@@ -52,27 +52,20 @@ class Patterns extends Component {
               rawMap={this.state.data}
               loading={this.state.isLoading}
               headers={[
-                <th key="th-ip">IP</th>,
                 <th key="th-fromto">Pattern</th>,
+                <th key="th-count">Count</th>,
               ]}
           >
-            <Col data={(datum, key) => key} width='20%' />
             <Col
-                // {ip:{fromUrl:{toUrl:X}}}
+                // {from, to, count}
                 data={(datum, key) =>
-                    Object.keys(datum[key]) // {fromUrl:{toUrl:X}}
-                        .map(fromUrl =>
-                          Object.keys(datum[key][fromUrl]) // {toUrl:X}
-                              .map(toUrl =>
-                                  <div key={key + '-' + fromUrl + '-' + toUrl}>
-                                    {fromUrl} --> {toUrl} [{datum[key][fromUrl][toUrl]}]
-                                    <hr/>
-                                  </div>
-                              )
-                        )
+                    <div key={key}>
+                      {datum[key].from} --> {datum[key].to}
+                    </div>
                 }
                 width='80%'
             />
+            <Col data={(datum, key) => datum[key].count} width='20%' />
           </Table>
         </section>
       </div>
