991b0bfcebd238b76abc07f4acec28caa57cc2be
dleidi <dleidi@suse.com>
2018-07-12 15:04:38 +0200

    Add more filters for patterns

--- dashboard/src/Patterns.js
+++ dashboard/src/Patterns.js
@@ -19,7 +19,7 @@ class Patterns extends Component {
       isLoading: false
     };
 
-    ['filterFrom', 'filterOutFrom', 'filterData']
+    ['filterFrom', 'filterOutFrom', 'filterTo', 'filterOutTo', 'filterData']
       .forEach(method => this[method] = this[method].bind(this));
   }
 
@@ -77,6 +77,38 @@ class Patterns extends Component {
     });
   }
 
+  filterTo(newCriteria) {
+    this.setState((prevState) => {
+      return (
+        {
+          criteria :
+            {
+              from: prevState.criteria.from,
+              fromOut: prevState.criteria.fromOut,
+              to: newCriteria,
+              toOut: prevState.criteria.toOut,
+            }
+        }
+      )
+    });
+  }
+
+  filterOutTo(newCriteria) {
+    this.setState((prevState) => {
+      return (
+        {
+          criteria :
+            {
+              from: prevState.criteria.from,
+              fromOut: prevState.criteria.fromOut,
+              to: prevState.criteria.to,
+              toOut: newCriteria,
+            }
+        }
+      )
+    });
+  }
+
   filterData(data) {
     if (this.state.criteria.from.length > 0) {
       try {
@@ -89,6 +121,22 @@ class Patterns extends Component {
     if (this.state.criteria.fromOut.length > 0) {
       try {
         data = data.filter(d => !d.from.match(this.state.criteria.fromOut));
+      }
+      catch (Exception) {
+        console.log('Invalid regex [' + Exception + ']');
+      }
+    }
+    if (this.state.criteria.to.length > 0) {
+      try {
+        data = data.filter(d => d.to.match(this.state.criteria.to));
+      }
+      catch (Exception){
+        console.log('Invalid regex [' + Exception + ']');
+      }
+    }
+    if (this.state.criteria.toOut.length > 0) {
+      try {
+        data = data.filter(d => !d.to.match(this.state.criteria.toOut));
       }
       catch (Exception) {
         console.log('Invalid regex [' + Exception + ']');
@@ -112,7 +160,7 @@ class Patterns extends Component {
               initialValue={this.state.criteria.from}
               placeholder='[use regex]'
               onChange={this.filterFrom}
-              label={'Filter-in by FromUrl'}
+              label={'Filter-in by "From"'}
               classStyle={'d-inline-block ' + (Utils.validateRegEx(this.state.criteria.from) ? '' : 'error')}
           />
           <TextInput
@@ -121,8 +169,26 @@ class Patterns extends Component {
               initialValue={this.state.criteria.fromOut}
               placeholder='[use regex]'
               onChange={this.filterOutFrom}
-              label={'Filter-out by FromUrl'}
+              label={'Filter-out by "From"'}
               classStyle={'d-inline-block ' + (Utils.validateRegEx(this.state.criteria.fromOut) ? '' : 'error')}
+          />
+          <TextInput
+              type='text'
+              name='criteriaTo'
+              initialValue={this.state.criteria.to}
+              placeholder='[use regex]'
+              onChange={this.filterTo}
+              label={'Filter-in by "To"'}
+              classStyle={'d-inline-block ' + (Utils.validateRegEx(this.state.criteria.to) ? '' : 'error')}
+          />
+          <TextInput
+              type='text'
+              name='criteriaToOut'
+              initialValue={this.state.criteria.toOut}
+              placeholder='[use regex]'
+              onChange={this.filterOutTo}
+              label={'Filter-out by "To"'}
+              classStyle={'d-inline-block ' + (Utils.validateRegEx(this.state.criteria.toOut) ? '' : 'error')}
           />
         </aside>
         <section>
